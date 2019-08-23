8b785561efd0f2f30ef380d5529e87e255e10d2d
Mark Sherry <marks@sortable.com>
2019-08-23 10:32:35 -0400

    Nicer formatting

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Branches
 
-`branches` is intended to supplement `git branch`. It lists all branches in the current 
+`branches` is intended to supplement `git branch`. It lists all branches in the current
 git repo, colour-coding and sorting them based on recency of the last update.
 
 I wrote this for myself to make it easier to context switch between branches, and make it
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
-extern crate git2;
-extern crate failure;
-extern crate colored;
 extern crate chrono;
 extern crate chrono_humanize;
+extern crate colored;
+extern crate failure;
+extern crate git2;
 #[macro_use]
 extern crate structopt;
 
-use git2::{Repository, BranchType};
-use std::env;
-use failure::Error;
-use chrono::{Utc, NaiveDateTime, TimeZone};
+use chrono::{NaiveDateTime, TimeZone, Utc};
 use colored::*;
+use failure::Error;
+use git2::{BranchType, Repository};
+use std::env;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name="branches", about="Slightly more useful than `git branches`")]
+#[structopt(name = "branches", about = "Slightly more useful than `git branches`")]
 struct Args {
     /// Sort in alphabetical order rather than by age
-    #[structopt(short = "a", long="alphabetical")]
+    #[structopt(short = "a", long = "alphabetical")]
     alphabetical: bool,
     /// Reverse sort order
-    #[structopt(short = "r", long="reverse")]
+    #[structopt(short = "r", long = "reverse")]
     reverse: bool,
 }
 
@@ -37,9 +37,16 @@ fn main() -> Result<(), Error> {
         let last_commit = branch.get().peel_to_commit()?;
 
         let commit_time = last_commit.time();
-        let message = last_commit.message().and_then(|m| m.split('\n').next()).unwrap_or_default().to_string();
+        let message = last_commit
+            .message()
+            .and_then(|m| m.split('\n').next())
+            .unwrap_or_default()
+            .to_string();
         let commit_time = NaiveDateTime::from_timestamp(commit_time.seconds() as i64, 0);
-        let commit_time = Utc.from_local_datetime(&commit_time).single().expect("Ambiguous timestamp");
+        let commit_time = Utc
+            .from_local_datetime(&commit_time)
+            .single()
+            .expect("Ambiguous timestamp");
 
         rv.push((branch, commit_time, message));
     }
@@ -50,21 +57,41 @@ fn main() -> Result<(), Error> {
         rv.reverse();
     }
 
-    for (branch, commit_time, message) in rv {
-        let line =  format!("{} [{}] {}", branch.name()?.unwrap_or(""), chrono_humanize::HumanTime::from(commit_time), message);
+    let rv: Vec<_> = rv.into_iter().map(|(b, commit_time, m)| (b, commit_time, format!("{}", chrono_humanize::HumanTime::from(commit_time)), m)).collect();
+
+    let branch_name_width = rv
+        .iter()
+        .filter_map(|row| row.0.name().ok().and_then(|x| x).map(str::len))
+        .max()
+        .unwrap_or(0);
+
+    let date_width = rv
+        .iter()
+        .map(|row| row.2.len())
+        .max()
+        .unwrap_or(0);
+
+    for (branch, commit_time, commit_str, message) in rv {
+        let line = format!(
+            "{:>width$} [{:>date_width$}] {}",
+            branch.name()?.unwrap_or(""),
+            commit_str,
+            message,
+            width = branch_name_width,
+            date_width = date_width
+        );
         let diff = now - commit_time;
         if diff.num_weeks() > 4 {
             println!("{}", line.red());
         } else if diff.num_weeks() >= 1 {
             println!("{}", line.yellow());
-        } else if diff.num_days () >= 3 {
+        } else if diff.num_days() >= 3 {
             println!("{}", line.bright_blue());
-        } else if diff.num_days () >= 1 {
+        } else if diff.num_days() >= 1 {
             println!("{}", line.green());
         } else {
             println!("{}", line.bright_green());
         }
-
     }
     Ok(())
 }
